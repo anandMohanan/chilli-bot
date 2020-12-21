836a5386475a4c3275e12b9b11855d1357fd615a
ananth <ananthkvmohanan@gmail.com>
2020-12-21 14:47:57 +0530

    roast update

--- commands/fun/roast.js
+++ commands/fun/roast.js
@@ -3,36 +3,30 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => {
   let user = message.mentions.users.first();
   if (message.mentions.users === message.author.username)
-    return message.reply('You can not compliment yourself');
+    return message.reply('You can not roast yourself');
   if (message.mentions.users.size < 1)
     return message.reply('You must mention someone to compliment them.');
   var roast = [
-    'People clap when they see you. They clap their hands over their eyes.',
-    'If I threw a stick, you’d leave, right?',
+    'You look like the kind of person who reads the terms and services then presses decline',
+    "You're just a hormonal banana",
+    'I’d make a joke about you having an extra chromosome, but it might be the first time its actually true.',
+    'The only time you see pussy is when you look in the mirror',
     'Keep rolling your eyes, you might eventually find a brain.',
-    'You’re as useful as the wings on a motorcycle.',
+    'You’re the guy they hire when the wife slept with the last pool boy.',
     'If your brain was dynamite, there wouldn’t be enough to blow your hat off.',
-    'Your kid is so annoying, he makes his Happy Meal cry.',
     'Hold still. I’m trying to imagine you with personality.',
-    'I forgot the world revolves around you. My apologies, how silly of me.',
-    'We were happily married for 3 months, but unfortunately, we have been married for 11 years.',
     'You are more disappointing than an unsalted pretzel.',
-    'The next time, I’ll knock you down instead of knocking your door.',
-    ' You’re the reason God created the middle finger.',
-    'Light travels faster than sound, which is why you seemed bright until you spoke.',
+    'Pretty sure this man uses cum as a hair gel',
+    "I'm trying to figure out why you even bothered to get dressed. Maybe he just got back from rinsing off his fleshlight in the kitchen sink.",
     'Well, the Jerk Store called, and they’re running out of you.',
-    'If I could give you that cookie back, I would. Nothing would make me happier than to throw it up, mash it into a cookie shape, and shove it down your throat.',
     'I’d prefer a battle of wits, but you appear unarmed.',
-
-    'You are the reason God created the middle finger.',
     'I thought of you today. It reminded me to take out the trash.',
-    'Someday, you’ll go far—and I really hope you stay there.',
-
+    'You’re so real. A real ass.',
+    "You're a less fuckable Ben Shapiro.",
+    "Didn't know a torn labia was a thing.",
     'I’m not a proctologist, but I know an asshole when I see one.',
-    'If you’re going to be two-faced, at least make one of them pretty.',
     'you are the human version of period cramps.',
-    'I love what you’ve done with your hair. How do you get it to come out of your nostrils like that?',
-    'You are so full of shit, the toilet’s jealous.',
+    "It's like you were drawn by an eight year old.",
   ];
   const roasts = roast[Math.floor(Math.random() * roast.length)];
   const embed = new Discord.MessageEmbed()
