e883cce5f958ad135003bc8e20c49608f40f9538
bruh711 <ananthkvmohanan@gmail.com>
2021-02-05 19:41:13 +0530

    added good roasts

--- commands/fun/roast.js
+++ commands/fun/roast.js
@@ -1,45 +1,29 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
 
 exports.run = async (client, message, args) => {
   let user = message.mentions.users.first();
   if (message.mentions.users === message.author.username)
-    return message.reply('You can not roast yourself');
+    return message.reply("You can not roast yourself");
   if (message.mentions.users.size < 1)
-    return message.reply('You must mention someone to compliment them.');
-  let roast = [
-    'You look like the kind of person who reads the terms and services then presses decline',
-    "You're just a hormonal banana",
-    'I’d make a joke about you having an extra chromosome, but it might be the first time its actually true.',
-    'The only time you see pussy is when you look in the mirror',
-    'Keep rolling your eyes, you might eventually find a brain.',
-    'You’re the guy they hire when the wife slept with the last pool boy.',
-    'If your brain was dynamite, there wouldn’t be enough to blow your hat off.',
-    'Hold still. I’m trying to imagine you with personality.',
-    'You are more disappointing than an unsalted pretzel.',
-    'Pretty sure this man uses cum as a hair gel',
-    "I'm trying to figure out why you even bothered to get dressed. Maybe he just got back from rinsing off his fleshlight in the kitchen sink.",
-    'Well, the Jerk Store called, and they’re running out of you.',
-    'I’d prefer a battle of wits, but you appear unarmed.',
-    'I thought of you today. It reminded me to take out the trash.',
-    'You’re so real. A real ass.',
-    "You're a less fuckable Ben Shapiro.",
-    "Didn't know a torn labia was a thing.",
-    'I’m not a proctologist, but I know an asshole when I see one.',
-    'you are the human version of period cramps.',
-    "It's like you were drawn by an eight year old.",
-  ];
-  const roasts = roast[Math.floor(Math.random() * roast.length)];
-  message.channel.send( user.username + ', ' + roasts);
+    return message.reply("You must mention someone to roast them.");
+
+  let body = await fetch(
+    "https://evilinsult.com/generate_insult.php?lang=en&type=json",
+  );
+  let roast = await body.json();
+
+  message.channel.send(user.username + ", " + roast.insult);
 };
 
 exports.help = {
-  name: 'roast',
-  description: 'roasts the mentioned user',
-  usage: 'roast [@user]',
-  example: 'roast @kevin malone',
+  name: "roast",
+  description: "roasts the mentioned user",
+  usage: "roast [@user]",
+  example: "roast @kevin malone",
 };
 
 exports.conf = {
-  aliases: ['r'],
+  aliases: ["r"],
   cooldown: 0,
 };
